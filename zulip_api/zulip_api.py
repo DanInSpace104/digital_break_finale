9ba5e81d7f601d0e841e9e9559f5fae42e96aaab
Wizard-C <44673127+Wizard-C@users.noreply.github.com>
2020-11-28 09:17:02 +0500

    Update zulip_api.py

--- zulip_api/zulip_api.py
+++ zulip_api/zulip_api.py
@@ -5,15 +5,21 @@ from django.conf import settings
 # Pass the path to your zuliprc file here.
 client = zulip.Client(config_file=str(settings.BASE_DIR / "zulip_api/.zuliprc"))
 
-# expert_mail = 'konshin104@gmail.com'
-# user_mail = 'BlueSkyC@mail.ru'
 
-# id_in_db_case = 104
-# name_case = 'Вариант докреентации кеверниции'
-# category = 'ДокРеентация'
+def zulip_create_stram(expert_mail, user_list, id_in_db_case, name_case, category):
+    '''
+    На вход получаем 5 аргументов, где:
+        expert_mail, является строкой и означает почту эксперта
+        user_list, список объектов юзера в виде list-а
+        id_in_db_case, ID кейса в БД (int)
+        name_case, Имя кейса (строка)
+        category, К какой категории относится кейс (строка)
+    Функция создает стрим в зулипе и добавляет туда всех юзеров и эксперта
+    '''
+    user_mail_list = []
+    for e in user_list:
+        user_mail_list.append(e.email)
 
-
-def zulip_create_stream(expert_mail, user_mail, claim_id, claim_name, category):
     result = client.call_endpoint(
         url='users?client_gravatar=true',
         method='GET',
@@ -22,15 +28,16 @@ def zulip_create_stream(expert_mail, user_mail, claim_id, claim_name, category):
     # Находим в динамике ID нужного эксперта, кого закрепить за стримом
     admins_id = []
     for user in result['members']:
-        if (user['email'] == expert_mail) or (user['email'] == user_mail):
-            expert_id = user['user_id']
-            print(user['user_id'])
+        if user['email'] == expert_mail:
+            admins_id.append(user['user_id'])
+        elif user['email'] in user_mail_list:
+            admins_id.append(user['user_id'])
 
     # Создать стрим
     result = client.add_subscriptions(
         streams=[
             {
-                'name': f'{claim_id}: {claim_name}',
+                'name': f'{id_in_db_case}: {name_case}',
                 'description': f'Категория: {category}',
             },
         ],
