ac3925748b9fe59aba47ec3a3d739ba7ac96f23e
Wizard-C <44673127+Wizard-C@users.noreply.github.com>
2020-11-28 18:57:30 +0500

    Create email.py

---
+++ email.py
@@ -0,0 +1,27 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+# Создается объект сообщения
+message_object = MIMEMultipart()
+
+# Данные авторизации
+password = 'Poiu0987'
+message_object['From'] = 'rossetiemail@gmail.com'
+
+def send_email(to, title, content):
+    message_object['To'] = to
+    message_object['Subject'] = title
+    message_text = content
+
+    # Добавляем тело сообщения
+    message_object.attach(MIMEText(message_text, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com: 587') 
+    server.starttls()
+
+    server.login(message_object['From'], password)
+    server.sendmail(message_object['From'], message_object['To'], message_object.as_string())
+    server.quit()
+
+    print("Successfully sent email to %s:" % (message_object['To']))
